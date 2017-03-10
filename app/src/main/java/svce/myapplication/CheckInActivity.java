b9101e5b8bd5172383ff260b82bd0667cb9625dd
Mathangi Sankaran <mathangisankaran@Mathangis-MacBook-Pro.local>
2017-03-10 23:00:29 +0530

    initial

---
+++ app/src/main/java/svce/myapplication/CheckInActivity.java
@@ -0,0 +1,87 @@
+package svce.myapplication;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class CheckInActivity extends ListActivity {
+
+
+    @Override
+    protected void onCreate(Bundle arg0) {
+        // TODO Auto-generated method stub
+        super.onCreate(arg0);
+
+        new GetPlaces(this, getListView()).execute();
+
+    }
+
+
+    class GetPlaces extends AsyncTask<Void, Void, Void> {
+
+        private ProgressDialog dialog;
+        private Context context;
+        private String[] placeName;
+        private String[] imageUrl;
+        private ListView listView;
+
+        public GetPlaces(Context context, ListView listView) {
+            // TODO Auto-generated constructor stub
+            this.context = context;
+            this.listView = listView;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // TODO Auto-generated method stub
+            super.onPostExecute(result);
+            dialog.dismiss();
+
+            listView.setAdapter(new ArrayAdapter<String>(context, android.R.layout.simple_expandable_list_item_1,placeName));
+        }
+
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+            super.onPreExecute();
+            dialog = new ProgressDialog(context);
+            dialog.setCancelable(true);
+            dialog.setMessage("Loading..");
+            dialog.isIndeterminate();
+            dialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // TODO Auto-generated method stub
+            PlacesService service = new PlacesService("AIzaSyAn6Z6ow4HCoSjh20SLbGstrXMtGar_IKo");
+            List<Place> findPlaces = service.findPlaces(28.632808,77.218276,"hospital");  // hospiral for hospital
+            // atm for ATM
+
+            placeName = new String[findPlaces.size()];
+            imageUrl = new String[findPlaces.size()];
+
+            for (int i = 0; i < findPlaces.size(); i++) {
+
+                Place placeDetail = findPlaces.get(i);
+                placeDetail.getIcon();
+
+                System.out.println(  placeDetail.getName());
+                placeName[i] =placeDetail.getName();
+
+                imageUrl[i] =placeDetail.getIcon();
+
+            }
+            return null;
+        }
+
+    }
+
+}
