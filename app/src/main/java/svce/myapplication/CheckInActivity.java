c6bebbd7a741c0d5b28d0b422ca8bb36bcf2f4e3
mathangisankaran <mathsank@gmail.com>
2017-03-11 10:01:42 +0530

    With location

--- app/src/main/java/svce/myapplication/CheckInActivity.java
+++ app/src/main/java/svce/myapplication/CheckInActivity.java
@@ -1,28 +1,125 @@
 package svce.myapplication;
 
+import android.Manifest;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
-public class CheckInActivity extends ListActivity {
-
+public class CheckInActivity extends ListActivity implements LocationListener {
+    private static final int REQUEST_PERMISSIONS = 20;
+    LocationManager locationManager;
+    String provider;
+    double longitude, latitude;
 
     @Override
     protected void onCreate(Bundle arg0) {
         // TODO Auto-generated method stub
         super.onCreate(arg0);
+// Getting LocationManager object
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+// Creating an empty criteria object
+        Criteria criteria = new Criteria();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                   REQUEST_PERMISSIONS);
+
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+
+        }
         new GetPlaces(this, getListView()).execute();
+        // Getting the name of the provider that meets the criteria
+        provider = locationManager.getBestProvider(criteria, false);
+
+        if (provider != null && !provider.equals("")) {
+
+            // Get the location from the given provider
+            Location location = locationManager.getLastKnownLocation(provider);
+
+
+            locationManager.requestLocationUpdates(provider, 20000, 1, this);
+
+            if(location!=null)
+                onLocationChanged(location);
+            else
+                Toast.makeText(getBaseContext(), "Location can't be retrieved", Toast.LENGTH_SHORT).show();
+
+        }
+
+
+        else{
+            Toast.makeText(getBaseContext(), "No Provider Found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSIONS: {
+                if ((grantResults.length > 0) && (grantResults[0] +
+                        grantResults[1]) == PackageManager.PERMISSION_GRANTED) {
+                    //Call whatever you want
+                    new GetPlaces(this, getListView()).execute();
+                } else {
+                    Toast.makeText(this,"Error",Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+       latitude =  location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
     }
 
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 
     class GetPlaces extends AsyncTask<Void, Void, Void> {
 
@@ -62,7 +159,7 @@ public class CheckInActivity extends ListActivity {
         protected Void doInBackground(Void... arg0) {
             // TODO Auto-generated method stub
             PlacesService service = new PlacesService("AIzaSyAn6Z6ow4HCoSjh20SLbGstrXMtGar_IKo");
-            List<Place> findPlaces = service.findPlaces(28.632808,77.218276,"hospital");  // hospiral for hospital
+            List<Place> findPlaces = service.findPlaces(latitude,longitude,"hospital");  // hospiral for hospital
             // atm for ATM
 
             placeName = new String[findPlaces.size()];
